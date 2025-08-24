3a0f13bd2b8f5eda810e7389d45a4e1dd5cd41dd
nova971z <justforwada@icloud.com>
2025-08-24 23:24:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const VERSION        = 'pt-v70
+const VERSION        = 'pt-v71
 '; // bump version pour diffuser la MAJ
 const CACHE_STATIC   = `pt-static-${VERSION}`;
 const CACHE_DYNAMIC  = `pt-dyn-${VERSION}`;
