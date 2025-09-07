076f5cc5b313793384896c8826ee4f29897dce24
nova971z <justforwada@icloud.com>
2025-09-07 20:23:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const VERSION        = 'pt-v112  './favicon.ico',
+const VERSION        = 'pt-v113  './favicon.ico',
   // icônes PWA (si certaines n’existent pas, elles seront ignorées proprement)
   './icons/icon-180.png',
   './icons/icon-192.png',
