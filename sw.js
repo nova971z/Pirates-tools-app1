a8d87a7ec8cc5526365fcbd20b6952cf4514bef6
nova971z <justforwada@icloud.com>
2025-09-11 01:28:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* sw.js — Pirates Tools (PWA) */
-const VERSION        = 'pt-v139';
+const VERSION        = 'pt-v140';
 const STATIC_CACHE   = `pt-static-${VERSION}`;
 const RUNTIME_CACHE  = `pt-runtime-${VERSION}`;
 const IMG_CACHE      = `pt-img-${VERSION}`;
